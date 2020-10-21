13b7a090fb1e8162242633d08b9ca7346482b3c0
Austin Doupnik <austindoupnik@gmail.com>
2020-10-20 20:33:45 -0700

    Add sphere

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
 use std::io;
 use std::borrow::BorrowMut;
 use crate::color::{write_color, Color};
-use crate::vec3::{unit_vector, Point3, Vec3};
+use crate::vec3::{unit_vector, Point3, Vec3, dot};
 use crate::ray::Ray;
 
 mod vec3;
 mod color;
 mod ray;
 
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> bool {
+    let oc = r.origin() - *center;
+    let a = dot(r.direction(), r.direction());
+    let b = 2.0 * dot(oc, r.direction());
+    let c = dot(oc, oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    return discriminant > 0.0;
+}
+
 fn ray_color(r: &Ray) -> Color {
+    if hit_sphere(&Point3 { e: [0.0, 0.0, -1.0] }, 0.5, r) {
+        return Color { e: [1.0, 0.0, 0.0] };
+    }
+
     let unit_direction = unit_vector(r.direction());
     let t = 0.5 * (unit_direction.y() + 1.0);
     (1.0 - t) * Color { e: [1.0, 1.0, 1.0] } + t * Color { e: [0.5, 0.7, 1.0] }
