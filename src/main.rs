ab6cb37c743cfeac38941a61d323e719cdee714c
Austin Doupnik <austindoupnik@gmail.com>
2020-10-27 19:03:53 -0700

    Add final render

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,12 @@ use crate::camera::Camera;
 use crate::color::{Color, write_color};
 use crate::hittable::{HitRecord, Hittable};
 use crate::hittable_list::HittableList;
-use crate::material::{Dielectric, Lambertian, Metal};
+use crate::material::{Dielectric, Lambertian, Metal, Material};
 use crate::point3::Point3;
 use crate::ray::Ray;
 use crate::sphere::Sphere;
 use crate::vec3::Vec3;
+use crate::random::{random_in_range, random};
 
 mod vec3;
 mod color;
@@ -45,32 +46,60 @@ fn ray_color(r: &Ray, world: &dyn Hittable, depth: u32) -> Color {
     }
 }
 
-fn main() {
-    let aspect_ratio = 16.0 / 9.0;
+fn random_scene() -> HittableList {
+    let mut world = HittableList::new();
 
-    let image_width = 400;
+    let ground_material = Lambertian::new(Color::new(0.5, 0.5, 0.5));
+    world.add(Rc::new(Sphere::new(Point3::new(0.0, -1000.0, 0.0), 1000.0, Rc::new(ground_material))));
+
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat = random();
+            let center = Point3::new(a as f64 + 0.9 * random(), 0.2, b as f64 + 0.9 * random());
+
+            if (center - Point3::new(4.0, 0.2, 0.0)).length() > 0.9 {
+                let sphere_material: Rc<dyn Material> = if choose_mat < 0.8 {
+                    let albedo = Color::random();
+                    Rc::new(Lambertian::new(albedo))
+                } else if choose_mat < 0.95 {
+                    let albedo = Color::random_in_range(0.5, 1.0);
+                    let fuzz = random_in_range(0.0, 0.5);
+                    Rc::new(Metal::new(albedo, fuzz))
+                } else {
+                    Rc::new(Dielectric::new(1.5))
+                };
+                world.add(Rc::new(Sphere::new(center, 0.2, sphere_material.clone())));
+            }
+        }
+    }
+
+    let material1 = Dielectric::new(1.5);
+    world.add(Rc::new(Sphere::new(Point3::new(0.0, 1.0, 0.0), 1.0, Rc::new(material1))));
+
+    let material2 = Lambertian::new(Color::new(0.4, 0.2, 0.1));
+    world.add(Rc::new(Sphere::new(Point3::new(-4.0, 1.0, 0.0), 1.0, Rc::new(material2))));
+
+    let material3 = Metal::new(Color::new(0.7, 0.6, 0.5), 0.0);
+    world.add(Rc::new(Sphere::new(Point3::new(4.0, 1.0, 0.0), 1.0, Rc::new(material3))));
+
+    world
+}
+
+fn main() {
+    let aspect_ratio = 3.0 / 2.0;
+
+    let image_width = 1200;
     let image_height = (image_width as f64 / aspect_ratio) as u32;
-    let samples_per_pixel = 100;
+    let samples_per_pixel = 500;
     let max_depth = 50;
 
-    let material_ground = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
-    let material_center = Rc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
-    let material_left = Rc::new(Dielectric::new(1.5));
-    let material_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 0.0));
+    let world = random_scene();
 
-    let mut world = HittableList::new();
-    world.add(Rc::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0, material_ground.clone())));
-    world.add(Rc::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5, material_center.clone())));
-    world.add(Rc::new(Sphere::new(Point3::new(-1.0, 0.0, -1.0), 0.5, material_left.clone())));
-    world.add(Rc::new(Sphere::new(Point3::new(-1.0, 0.0, -1.0), -0.45, material_left.clone())));
-    world.add(Rc::new(Sphere::new(Point3::new(1.0, 0.0, -1.0), 0.5, material_right.clone())));
-    let world = world;
+    let lookfrom = Point3::new(13.0, 2.0, 3.0);
+    let lookat = Point3::new(0.0, 0.0, 0.0);
+    let dist_to_focus = 10.0;
 
-    let lookfrom = Point3::new(3.0, 3.0, 2.0);
-    let lookat = Point3::new(0.0, 0.0, -1.0);
-    let dist_to_focus = (lookfrom - lookat).length();
-
-    let cam = Camera::new(lookfrom, lookat, Vec3::new(0.0, 1.0, 0.0), 20.0, aspect_ratio, 2.0, dist_to_focus);
+    let cam = Camera::new(lookfrom, lookat, Vec3::new(0.0, 1.0, 0.0), 20.0, aspect_ratio, 0.1, dist_to_focus);
 
     print!("P3\n{} {}\n255\n", image_width, image_height);
 
